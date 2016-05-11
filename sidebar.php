b71f039848b57b36e3f619de9e946321ee8cfc5f
Péricles Feltrin <periclesfeltrin@Pericless-MacBook-Pro.local>
2016-05-11 17:12:51 -0300

    Alteração ordens cards

--- sidebar.php
+++ sidebar.php
@@ -1,22 +1,4 @@
-
 <aside>
-  <div class="sidebar-widget">
-  	<h3 class="sidebar-title">Sobre Nós</h3>
-  	<div class="widget-container widget-about">
-  		<a href="post.html"><img src="images/smallLogo.JPG" alt=""></a>
-  		<h4>Enem Tô Top</h4>
-  		<div class="author-title">Porque nós acreditamos em você</div>
-  		<p>O Enem Tô Top é pra você que sofre com a vida de vestibulando! A gente já passou por isso e pra te ajudar criamos esse portal, pra você encontrar dicas de como sobreviver a essa fase crítica e facilitar seu caminho pro sucesso!</p>
-  	</div>
-  </div>
-   <div class="sidebar-widget">
-    <h3 class="sidebar-title">Baixe Logo Nosso Ebook</h3>
-    <div class="widget-container widget-about">
-      <a href="post.php?id=5"><img src="images/noticia/ebook_guia_recursos.png" alt=""></a>
-      <br><br>
-      <a href="post.php?id=5" class="btn btn-success btn-block"><b>BAIXAR EBOOK AGORA!</b></a>
-    </div>
-  </div>
   <div class="sidebar-widget">
     <h3 class="sidebar-title">Ganhe 1 Mês Grátis de Aulas</h3>
     <div class="widget-container widget-about">
@@ -25,16 +7,33 @@
       <a href="post.php?id=12" class="btn btn-success btn-block"><b>EU QUERO GANHAR!</b></a>
     </div>
   </div>
-
+  <div class="sidebar-widget">
+    <h3 class="sidebar-title">Baixe Logo Nosso Ebook</h3>
+    <div class="widget-container widget-about">
+      <a href="post.php?id=5"><img src="images/noticia/ebook_guia_recursos.png" alt=""></a>
+      <br><br>
+      <a href="post.php?id=5" class="btn btn-success btn-block"><b>BAIXAR EBOOK AGORA!</b></a>
+    </div>
+  </div>
+  <div class="sidebar-widget">
+    <h3 class="sidebar-title">Sobre Nós</h3>
+    <div class="widget-container widget-about">
+      <a href="post.html"><img src="images/smallLogo.JPG" alt=""></a>
+      <h4>Enem Tô Top</h4>
+      <div class="author-title">Porque nós acreditamos em você</div>
+      <p>O Enem Tô Top é pra você que sofre com a vida de vestibulando! A gente já passou por isso e pra te ajudar criamos esse portal, pra você encontrar dicas de como sobreviver a essa fase crítica e facilitar seu caminho pro sucesso!</p>
+    </div>
+  </div>
+  
   <?php include "quero_receber_atualizacao.html";?>
   
   <div class="sidebar-widget">
-  	<div class="widget-container">
-  		<div class="widget-container">
+    <div class="widget-container">
+      <div class="widget-container">
             <div class="fb-page" data-href="https://www.facebook.com/enemtotop/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/enemtotop/"><a href="https://www.facebook.com/enemtotop/">Enem tô top</a></blockquote></div></div>
             <div id="fb-root"></div>
             <script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.6";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script>
-  		</div>
-  	</div>
+      </div>
+    </div>
   </div>
 </aside>
